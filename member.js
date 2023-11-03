84c53fe992acc0d0a2d378d78d442a1d885a59eb
keanshubham <126885008+keanshubham@users.noreply.github.com>
2023-11-03 13:44:22 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember()
+{
+    var member = {
+        name: "John",
+        age: 30,
+        skills: ["JS", "PHP", "Java"]
+    };
+
+    var skills = member.skills;
+    console.log(skills);
+}
+
